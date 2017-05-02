80c00fa6323a7e9d45a7a1be1595664b5a6e74bc
Erik Cederstrand <erik@cederstrand.dk>
2017-05-02 23:20:56 +0200

    Convert classes to FIELDS

--- exchangelib/recurrence.py
+++ exchangelib/recurrence.py
@@ -1,6 +1,12 @@
-from .ewsdatetime import EWSDate, EWSDateTime
+import logging
+
+from six import string_types
+
+from .fields import IntegerField, EnumField, EnumListField, DateField, DateTimeField, EWSElementField
 from .folders import EWSElement
-from .util import create_element, add_xml_child
+from .properties import ItemId
+
+log = logging.getLogger(__name__)
 
 
 # DayOfWeekIndex enum. See https://msdn.microsoft.com/en-us/library/office/aa581350(v=exchg.150).aspx
@@ -46,6 +52,33 @@ EXTRA_WEEKDAY_OPTIONS = (DAY, WEEK_DAY, WEEKEND_DAY)
 WEEKDAYS = WEEKDAY_NAMES + EXTRA_WEEKDAY_OPTIONS
 
 
+class ExtraWeekdaysField(EnumListField):
+    def __init__(self, *args, **kwargs):
+        kwargs['enum'] = WEEKDAYS
+        super(ExtraWeekdaysField, self).__init__(*args, **kwargs)
+
+    def clean(self, value, version=None):
+        # Pass EXTRA_WEEKDAY_OPTIONS as single string or integer value
+        if isinstance(value, string_types):
+            if value not in EXTRA_WEEKDAY_OPTIONS:
+                raise ValueError(
+                    "Single value '%s' on field '%s' must be one of %s" % (value, self.name, EXTRA_WEEKDAY_OPTIONS))
+            value = [self.enum.index(value) + 1]
+        elif isinstance(value, self.value_cls):
+            value = [value]
+        else:
+            value = list(value)  # Convert to something we can index
+            for i, v in enumerate(value):
+                if isinstance(value, string_types):
+                    if v not in WEEKDAY_NAMES:
+                        raise ValueError(
+                            "List value '%s' on field '%s' must be one of %s" % (v, self.name, WEEKDAY_NAMES))
+                    value[i] = self.enum.index(v) + 1
+                elif isinstance(v, self.value_cls) and not 1 <= v <= 7:
+                    raise ValueError("List value '%s' on field '%s' must be in range 1 -> 7" % (v, self.name))
+        return super(ExtraWeekdaysField, self).clean(value, version=version)
+
+
 class Pattern(EWSElement):
     pass
 
@@ -54,71 +87,37 @@ class AbsoluteYearlyPattern(Pattern):
     # MSDN: https://msdn.microsoft.com/en-us/library/office/aa564242(v=exchg.150).aspx
     ELEMENT_NAME = 'AbsoluteYearlyRecurrence'
 
-    def __init__(self, month, day_of_month):
-        """
-
-        :param month: Month number, in range 1 -> 12
-        :param day_of_month: The day of month of an occurrence, in range 1 -> 31. If a particular month has less days
-        than the day_of_month value, the last day in the month is assumed
-        """
-        assert 1 <= month <= 12
-        assert 1 <= day_of_month <= 31
-        self.month = month
-        self.day_of_month = day_of_month
-
-    def to_xml(self, version):
-        self.clean()
-        entry = create_element(self.request_tag())
-        add_xml_child(entry, 't:DayOfMonth', self.day_of_month)
-        add_xml_child(entry, 't:Month', MONTHS[self.month-1])
-        return entry
+    FIELDS = [
+        # The month of the year, from 1 - 12
+        EnumField('month', field_uri='t:Month', enum=MONTHS),
+        # The day of month of an occurrence, in range 1 -> 31. If a particular month has less days than the day_of_month
+        # value, the last day in the month is assumed
+        IntegerField('day_of_month', field_uri='t:DayOfMonth', min=1, max=31),
+    ]
 
     def __str__(self):
-        return 'Occurs on the %s. day of the %s. month of the year' % (self.day_of_month, self.month)
+        return 'Occurs on the %s. day of %s' % (self.day_of_month, MONTHS[self.month-1])
 
 
 class RelativeYearlyPattern(Pattern):
     # MSDN: https://msdn.microsoft.com/en-us/library/office/bb204113(v=exchg.150).aspx
     ELEMENT_NAME = 'RelativeYearlyRecurrence'
 
-    def __init__(self, month, week_number, weekdays):
-        """
-
-        :param month: Month of the year, in range 1 -> 12
-        :param week_number: Week number of the month, in range 1 -> 5. If 5 is specified, this assumes the last week of
-        the month for months that have only 4 weeks
-        :param weekdays: List of valid ISO 8601 weekdays, as list of numbers in range 1 -> 7 (1 being Monday)
-        than the day_of_month value, the last day in the month is assumed. Alternatively, weekdays can be one of the
-        DAY, WEEK_DAY or WEEKEND_DAY consts which is interpreted as the first day, weekday, or weekend day in the month,
-        respectively.
-        """
-        assert 1 <= month <= 12
-        assert 1 <= week_number <= 53
-        if isinstance(weekdays, str):
-            assert weekdays in EXTRA_WEEKDAY_OPTIONS
-        else:
-            assert len(weekdays) > 0
-            for weekday in weekdays:
-                assert 1 <= weekday <= 7
-        self.month = month
-        self.week_number = week_number
-        self.weekdays = weekdays
-
-    def to_xml(self, version):
-        self.clean()
-        if isinstance(self.weekdays, str):
-            days_of_week = self.weekdays
-        else:
-            days_of_week = ' '.join(WEEKDAYS[i-1] for i in sorted(self.weekdays))
-        entry = create_element(self.request_tag())
-        add_xml_child(entry, 't:DaysOfWeek', days_of_week)
-        add_xml_child(entry, 't:DayOfWeekIndex', WEEK_NUMBERS[self.week_number-1])
-        add_xml_child(entry, 't:Month', MONTHS[self.month-1])
-        return entry
+    FIELDS = [
+        # The month of the year, from 1 - 12
+        EnumField('month', field_uri='t:Month', enum=MONTHS),
+        # Week number of the month, in range 1 -> 5. If 5 is specified, this assumes the last week of the month for
+        # months that have only 4 weeks
+        EnumField('week_number', field_uri='t:DayOfWeekIndex', enum=WEEK_NUMBERS),
+        # List of valid ISO 8601 weekdays, as list of numbers in range 1 -> 7 (1 being Monday). Alternatively, weekdays
+        # can be one of the DAY (or 8), WEEK_DAY (or 9) or WEEKEND_DAY (or 10) consts which is interpreted as the first
+        # day, weekday, or weekend day in the month, respectively.
+        ExtraWeekdaysField('weekdays', field_uri='t:DaysOfWeek'),
+    ]
 
     def __str__(self):
-        return 'Occurs on weekdays %s in the %s. week of the %s. month of the year' % (
-            self.weekdays, self.week_number, self.month
+        return 'Occurs on weekdays %s in the %s week of %s' % (
+            ', '.join(WEEKDAYS[i - 1] for i in self.weekdays), WEEK_NUMBERS[self.week_number-1], MONTHS[self.month-1]
         )
 
 
@@ -126,24 +125,13 @@ class AbsoluteMonthlyPattern(Pattern):
     # MSDN: https://msdn.microsoft.com/en-us/library/office/aa493844(v=exchg.150).aspx
     ELEMENT_NAME = 'AbsoluteMonthlyRecurrence'
 
-    def __init__(self, interval, day_of_month):
-        """
-
-        :param interval: Interval, in months, in range 1 -> 99
-        :param day_of_month: The day of month of an occurrence, in range 1 -> 31. If a particular month has less days
-        than the day_of_month value, the last day in the month is assumed
-        """
-        assert 1 <= interval <= 99
-        assert 1 <= day_of_month <= 31
-        self.interval = interval
-        self.day_of_month = day_of_month
-
-    def to_xml(self, version):
-        self.clean()
-        entry = create_element(self.request_tag())
-        add_xml_child(entry, 't:Interval', self.interval)
-        add_xml_child(entry, 't:DayOfMonth', self.day_of_month)
-        return entry
+    FIELDS = [
+        # Interval, in months, in range 1 -> 99
+        IntegerField('interval', field_uri='t:Interval', min=1, max=99),
+        # The day of month of an occurrence, in range 1 -> 31. If a particular month has less days than the day_of_month
+        # value, the last day in the month is assumed
+        IntegerField('day_of_month', field_uri='t:DayOfMonth', min=1, max=31),
+    ]
 
     def __str__(self):
         return 'Occurs on the %s. day of every %s. month' % (self.day_of_month, self.interval)
@@ -153,44 +141,21 @@ class RelativeMonthlyPattern(Pattern):
     # MSDN: https://msdn.microsoft.com/en-us/library/office/aa564558(v=exchg.150).aspx
     ELEMENT_NAME = 'RelativeMonthlyRecurrence'
 
-    def __init__(self, interval, week_number, weekdays):
-        """
-
-        :param interval: Interval, in months, in range 1 -> 99
-        :param week_number: Week number of the month, in range 1 -> 5. If 5 is specified, this assumes the last week of
-        the month for months that have only 4 weeks
-        :param weekdays: List of valid ISO 8601 weekdays, as list of numbers in range 1-> 7 (1 being Monday)
-        than the day_of_month value, the last day in the month is assumed. Alternatively, weekdays can be one of the
-        DAY, WEEK_DAY or WEEKEND_DAY consts which is interpreted as the first day, weekday, or weekend day in the month,
-        respectively.
-        """
-        assert 1 <= week_number <= 53
-        assert len(weekdays) > 0
-        if isinstance(weekdays, str):
-            assert weekdays in EXTRA_WEEKDAY_OPTIONS
-        else:
-            assert len(weekdays) > 0
-            for weekday in weekdays:
-                assert 1 <= weekday <= 7
-        self.interval = interval
-        self.week_number = week_number
-        self.weekdays = weekdays
-
-    def to_xml(self, version):
-        self.clean()
-        if isinstance(self.weekdays, str):
-            days_of_week = self.weekdays
-        else:
-            days_of_week = ' '.join(WEEKDAYS[i-1] for i in sorted(self.weekdays))
-        entry = create_element(self.request_tag())
-        add_xml_child(entry, 't:Interval', self.interval)
-        add_xml_child(entry, 't:DaysOfWeek', days_of_week)
-        add_xml_child(entry, 't:DayOfWeekIndex', WEEK_NUMBERS[self.week_number-1])
-        return entry
+    FIELDS = [
+        # Interval, in months, in range 1 -> 99
+        IntegerField('interval', field_uri='t:Interval', min=1, max=99),
+        # Week number of the month, in range 1 -> 5. If 5 is specified, this assumes the last week of the month for
+        # months that have only 4 weeks.
+        EnumField('week_number', field_uri='t:DayOfWeekIndex', enum=WEEK_NUMBERS),
+        # List of valid ISO 8601 weekdays, as list of numbers in range 1 -> 7 (1 being Monday). Alternatively, weekdays
+        # can be one of the DAY (or 8), WEEK_DAY (or 9) or WEEKEND_DAY (or 10) consts which is interpreted as the first
+        # day, weekday, or weekend day in the month, respectively.
+        ExtraWeekdaysField('weekdays', field_uri='t:DaysOfWeek'),
+    ]
 
     def __str__(self):
-        return 'Occurs on weekdays %s in the %s. week of every %s. month' % (
-            self.weekdays, self.week_number, self.interval
+        return 'Occurs on weekdays %s in the %s week of every %s. month' % (
+            ', '.join(WEEKDAYS[i - 1] for i in self.weekdays), WEEK_NUMBERS[self.week_number-1], self.interval
         )
 
 
@@ -198,145 +163,82 @@ class WeeklyPattern(Pattern):
     # MSDN: https://msdn.microsoft.com/en-us/library/office/aa563500(v=exchg.150).aspx
     ELEMENT_NAME = 'WeeklyRecurrence'
 
-    def __init__(self, interval, weekdays):
-        """
-
-        :param interval: Interval, in weeks, in range 1 -> 99
-        :param weekdays: List of valid ISO 8601 weekdays, as list of numbers in range 1-> 7 (1 being Monday)
-        """
-        assert 1 <= interval <= 99
-        assert len(weekdays) > 0
-        for weekday in weekdays:
-            assert 1 <= weekday <= 7
-        self.interval = interval
-        self.weekdays = weekdays
-
-    def to_xml(self, version):
-        self.clean()
-        entry = create_element(self.request_tag())
-        add_xml_child(entry, 't:Interval', self.interval)
-        add_xml_child(entry, 't:DaysOfWeek', ' '.join(WEEKDAYS[i-1] for i in sorted(self.weekdays)))
-        add_xml_child(entry, 't:FirstDayOfWeek', WEEKDAYS[0])  # TODO: forcing Monday may not be correct. Use account.locale?
-        return entry
+    FIELDS = [
+        # Interval, in weeks, in range 1 -> 99
+        IntegerField('interval', field_uri='t:Interval', min=1, max=99),
+        # List of valid ISO 8601 weekdays, as list of numbers in range 1 -> 7 (1 being Monday)
+        EnumListField('weekdays', field_uri='t:DaysOfWeek', enum=WEEKDAYS),
+        # The first day of the week. Defaults to Monday
+        EnumField('first_day_of_week', field_uri='t:FirstDayOfWeek', enum=WEEKDAYS, default=1),
+    ]
 
     def __str__(self):
-        return 'Occurs on weekdays %s of every %s. week' % (self.weekdays, self.interval)
+        return 'Occurs on weekdays %s of every %s. week where the first day of the week is %s' % (
+            ', '.join(WEEKDAYS[i - 1] for i in self.weekdays), self.interval, WEEKDAYS[self.first_day_of_week-1]
+        )
 
 
 class DailyPattern(Pattern):
     # MSDN: https://msdn.microsoft.com/en-us/library/office/aa563228(v=exchg.150).aspx
     ELEMENT_NAME = 'DailyRecurrence'
 
-    def __init__(self, interval):
-        """
-
-        :param interval: Interval, in days, in range 1 -> 999
-        """
-        assert 1 <= interval <= 999
-        self.interval = interval
-
-    def to_xml(self, version):
-        self.clean()
-        entry = create_element(self.request_tag())
-        add_xml_child(entry, 't:Interval', self.interval)
-        return entry
+    FIELDS = [
+        # Interval, in days, in range 1 -> 999
+        IntegerField('interval', field_uri='t:Interval', min=1, max=999),
+    ]
 
     def __str__(self):
-        return 'Occurs every %s. day' % (self.interval)
+        return 'Occurs every %s. day' % self.interval
 
 
 class NoEndPattern(Pattern):
     # MSDN: https://msdn.microsoft.com/en-us/library/office/aa564699(v=exchg.150).aspx
     ELEMENT_NAME = 'NoEndRecurrence'
 
-    def __init__(self, start):
-        """
-
-        :param start:  Start date, as EWSDate
-        """
-        assert isinstance(start, EWSDate)
-        self.start = start
-
-    def to_xml(self, version):
-        self.clean()
-        entry = create_element(self.request_tag())
-        add_xml_child(entry, 't:StartDate', self.start)
-        return entry
+    FIELDS = [
+        # Start date, as EWSDate
+        DateField('start', field_uri='t:StartDate'),
+    ]
 
 
 class EndDatePattern(Pattern):
     # MSDN: https://msdn.microsoft.com/en-us/library/office/aa564536(v=exchg.150).aspx
     ELEMENT_NAME = 'EndDateRecurrence'
 
-    def __init__(self, start, end):
-        """
-
-        :param start: Start date, as EWSDate
-        :param end: End date, as EWSDate
-        """
-        assert isinstance(start, EWSDate)
-        assert isinstance(end, EWSDate)
-        self.start = start
-        self.end = end
-
-    def to_xml(self, version):
-        self.clean()
-        entry = create_element(self.request_tag())
-        add_xml_child(entry, 't:StartDate', self.start)
-        add_xml_child(entry, 't:EndDate', self.end)
-        return entry
+    FIELDS = [
+        # Start date, as EWSDate
+        DateField('start', field_uri='t:StartDate'),
+        # End date, as EWSDate
+        DateField('end', field_uri='t:EndDate'),
+    ]
 
 
 class NumberedPattern(Pattern):
     # MSDN: https://msdn.microsoft.com/en-us/library/office/aa580960(v=exchg.150).aspx
     ELEMENT_NAME = 'NumberedRecurrence'
 
-    def __init__(self, start, number):
-        """
-
-        :param start: Start date, as EWSDate
-        :param number: The number of occurrences in this pattern
-        """
-        assert isinstance(start, EWSDate)
-        assert isinstance(number, int)
-        self.start = start
-        self.number = number
-
-    def to_xml(self, version):
-        self.clean()
-        entry = create_element(self.request_tag())
-        add_xml_child(entry, 't:StartDate', self.start)
-        add_xml_child(entry, 't:NumberOfOccurrences', self.number)
-        return entry
+    FIELDS = [
+        # Start date, as EWSDate
+        DateField('start', field_uri='t:StartDate'),
+        # The number of occurrences in this pattern
+        IntegerField('number', field_uri='t:NumberOfOccurrences', min=0),
+    ]
 
 
 class Occurrence(EWSElement):
     # MSDN: https://msdn.microsoft.com/en-us/library/office/aa565603(v=exchg.150).aspx
     ELEMENT_NAME = 'Occurrence'
 
-    def __init__(self, item, start, end, original_start):
-        """
-
-        :param item: The item_id and changekey of a modified item occurrence
-        :param start: The modified start time of the item, as EWSDateTime
-        :param end: The modified end time of the item, as EWSDateTime
-        :param original_start: The original start time of the item, as EWSDateTime
-        """
-        assert isinstance(start, EWSDateTime)
-        assert isinstance(end, EWSDateTime)
-        assert isinstance(original_start, EWSDateTime)
-        self.item = item
-        self.start = start
-        self.end = end
-        self.original_start = original_start
-
-    def to_xml(self, version):
-        self.clean()
-        entry = create_element(self.request_tag())
-        add_xml_child(entry, 't:Start', self.start)
-        add_xml_child(entry, 't:End', self.end)
-        add_xml_child(entry, 't:OriginalStart', self.original_start)
-        return entry
+    FIELDS = [
+        # The item_id and changekey of a modified item occurrence
+        EWSElementField('item', value_cls=ItemId, is_read_only=True),
+        # The modified start time of the item, as EWSDateTime
+        DateTimeField('start', field_uri='t:Start'),
+        # The modified end time of the item, as EWSDateTime
+        DateTimeField('end', field_uri='t:End'),
+        # The original start time of the item, as EWSDateTime
+        DateTimeField('original_start', field_uri='t:OriginalStart'),
+    ]
 
 # Container elements:
 # 'ModifiedOccurrences'
@@ -357,14 +259,10 @@ class DeletedOccurrence(EWSElement):
     # MSDN: https://msdn.microsoft.com/en-us/library/office/aa566477(v=exchg.150).aspx
     ELEMENT_NAME = 'DeletedOccurrence'
 
-    def __init__(self, start):
-        self.start = start
-
-    def to_xml(self, version):
-        self.clean()
-        entry = create_element(self.request_tag())
-        add_xml_child(entry, 't:Start', self.start)
-        return entry
+    FIELDS = [
+        # The modified start time of the item, as EWSDateTime
+        DateTimeField('start', field_uri='t:Start'),
+    ]
 
 
 class Recurrence(EWSElement):
